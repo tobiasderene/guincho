816877471dfe6aac334f9c69da8e493d2a3f2c12
tobiasderene <133263634+tobiasderene@users.noreply.github.com>
2025-08-11 13:24:19 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home.jsx";
 import Category from "./pages/Category.jsx";
@@ -10,9 +10,15 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path="/inicio" element={<Home />} />
+        {/* Ruta principal */}
+        <Route path="/" element={<Home />} />
+
+        {/* Rutas específicas */}
         <Route path="/categorias" element={<Category />} />
         <Route path="/iniciarsesion" element={<Login />} />
+
+        {/* Catch-all para redirección */}
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
   );
