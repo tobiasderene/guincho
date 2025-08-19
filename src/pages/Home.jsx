20aca7c76f56bf4aaf05d58e75366802630229c0
KanutoMR <marcelo.rodriguez@upa.edu.py>
2025-08-19 13:02:04 -0300

    home fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/Home.css';
-import Header from "./components/Header";
+import Header from "../components/Header";
 import BotonPlus from '../components/BotonPlus';
 import { useAuth } from '../hooks/useAuth';
 
