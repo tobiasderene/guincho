1cdd143b2a6ddb9a5ea9ea1985f3d47c61e9097e
KanutoMR <marcelo.rodriguez@upa.edu.py>
2025-08-26 12:47:04 -0300

    move google analytics api

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import { useEffect } from "react";
 import { AuthProvider } from "./context/AuthContext";
 import Header from "./components/Header";
 import Home from "./pages/Home.jsx";
@@ -12,34 +11,10 @@ import Post from "./pages/Post.jsx";
 
 function AppContent() {
   const location = useLocation();
-
-  useEffect(() => {
-    // Cargar el script externo
-    const script = document.createElement("script");
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-J2PFQF4155";
-    script.async = true;
-    document.head.appendChild(script);
-
-    // Definir la función global gtag
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){window.dataLayer.push(arguments);}
-    window.gtag = gtag; // lo dejamos global para reutilizarlo
-    gtag("js", new Date());
-    gtag("config", "G-J2PFQF4155");
-  }, []);
-
-  // 👇 cada vez que cambia la ruta, avisamos a GA
-  useEffect(() => {
-    if (window.gtag) {
-      window.gtag("config", "G-J2PFQF4155", {
-        page_path: location.pathname,
-      });
-    }
-  }, [location]);
-
+  
   // Rutas donde NO queremos mostrar el header
   const hideHeaderRoutes = ["/iniciarsesion", "/registrarse", "/crearpost"];
-
+  
   return (
     <>
       {!hideHeaderRoutes.includes(location.pathname) && <Header />}
